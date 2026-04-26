58af289ac0130ffa971e5df6d24e5415b6c49d2d
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-26 19:41:42 -0300

    Adicionado lógica de resposta para o endpoint de cadastro de clientes

--- Agenda-Pro-Main/src/main/java/com/martins/Agenda_Pro/controller/InsertClient.java
+++ Agenda-Pro-Main/src/main/java/com/martins/Agenda_Pro/controller/InsertClient.java
@@ -1,19 +1,47 @@
 package com.martins.Agenda_Pro.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.martins.Agenda_Pro.controller.entities.InsertClientResponse;
+import com.martins.Agenda_Pro.errors.MainError;
+import com.martins.Agenda_Pro.repository.clients.table.Client;
+import com.martins.Agenda_Pro.responses.ErrorResponse;
+import com.martins.Agenda_Pro.responses.ResponseModel;
+import com.martins.Agenda_Pro.services.insert_client.InsertClientDTO;
+import com.martins.Agenda_Pro.services.insert_client.InsertClientService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class InsertClient {
 
-  @PostMapping("/create/client")
-  public Map<String, String> InsertClientController() {
+  private final InsertClientService service;
 
-    return new HashMap<>();
+  public InsertClient(InsertClientService service) {
+    this.service = service;
+  }
+
+  @PostMapping("/create/client")
+  public ResponseEntity<ResponseModel> InsertClientController(@Valid @RequestBody InsertClientDTO request) {
+    try {
+      Client insert = service.salvar(request);
+
+      return ResponseEntity
+          .ok(new InsertClientResponse("sucesso", "Cliente adicionado com sucesso"));
+    } catch (MainError e) {
+      return ResponseEntity
+          .status(e.getStatus())
+          .body(new ErrorResponse("error", e.getMessage()));
+
+    } catch (Exception e) {
+      return ResponseEntity
+          .status(500)
+          .body(new ErrorResponse("error", e.getMessage()));
+    }
   }
 }
