8ca4f1e9b814f4b13ae47940e1ecb9319eed20c5
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-26 18:17:25 -0300

    Alterado coleta de contexto do teste de integração

--- Agenda-Pro-Main/src/test/java/com/martins/Agenda_Pro/login/controllers/integration/LoginControllerIntegrationTest.java
+++ Agenda-Pro-Main/src/test/java/com/martins/Agenda_Pro/login/controllers/integration/LoginControllerIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.martins.Agenda_Pro.login.controllers.integration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,8 +14,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 public class LoginControllerIntegrationTest {
 
-  @Autowired
   private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @BeforeEach
+  void setup() {
+    this.mockMvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .build();
+  }
 
   @Test
   void testAuthenticateUser() throws Exception {
@@ -30,24 +42,17 @@ public class LoginControllerIntegrationTest {
   }
 
   @Test
-  void testRateLimitterBloqueiaAposLimit() throws Exception {
+  void testUsuarioNaoEncontrado() throws Exception {
     String json = """
-            {
-                "email": "admin@email.com",
-                "password": "admin"
-            }
+              {
+                  "email": "usuario@invalido.com",
+                  "password": "admin"
+              }
         """;
-
-    for (int i = 0; i < 2; i++) {
-      mockMvc.perform(post("/api/login")
-          .contentType("application/json")
-          .content(json))
-          .andExpect(status().isOk());
-    }
 
     mockMvc.perform(post("/api/login")
         .contentType("application/json")
         .content(json))
-        .andExpect(status().isTooManyRequests());
+        .andExpect(status().isNotFound());
   }
 }
