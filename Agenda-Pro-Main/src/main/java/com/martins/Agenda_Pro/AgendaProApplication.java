3f227065e6351274b2646d1a30489f938e940dbf
Ivan-Martins-DevProjects <Ivan_G.Martins@outlook.com>
2026-04-29 00:39:42 -0300

    Alterações estruturais

--- Agenda-Pro-Main/src/main/java/com/martins/Agenda_Pro/AgendaProApplication.java
+++ Agenda-Pro-Main/src/main/java/com/martins/Agenda_Pro/AgendaProApplication.java
@@ -2,8 +2,10 @@ package com.martins.Agenda_Pro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AgendaProApplication {
 
   public static void main(String[] args) {
